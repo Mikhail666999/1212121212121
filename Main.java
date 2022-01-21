8a51f056588a54931b452c1217ccb8e8bffc99a1
Mika <61342907+Mikhail666999@users.noreply.github.com>
2022-01-21 20:19:44 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 class Main{
-
-	public static String 
-
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        int max = 10000;
+		System.out.println(Math.random() * max);
+
     }
 }
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 catalog="/home/mika/1MyAll1/projects/121212121212"
 file_name_with_extension="Main.java"
-file_name_without_expansion="Main"
+file_name_without_extension="Main"
 
 echo "######################### Начало ####################################################################" >> log.txt
 echo "Время начала: `date`" >> log.txt
@@ -10,15 +10,16 @@ echo -e "" >> log.txt
 cd "$catalog"
 echo "Переходим в каталог \""$catalog"\"" >> log.txt
 read -p "Отчистить экран [0/1]: " x
-echo -e ""
+echo $x
 echo "Спрашиваем пользователя хочет ли он отчистить консоль" >> log.txt
+echo -e ""
 
 if [[ 0 = "$x" ]]; then
 	echo "Отказался" >> log.txt
 elif [[ 1 = "$x" ]]; then 
 	echo "Согласился" >> log.txt
-	clear
 	echo "Отчищаем консоль" >> log.txt
+	clear
 else 
 	echo "Вы ввели неправильное число..." >> log.txt
 	echo "#####################-----Конец-----##############################" >> log.txt
@@ -28,7 +29,7 @@ else
 fi
 echo -e "" >> log.txt
 echo "--------------------------------------------------------------------"
-echo "Ошибки \"javac "$namefiledlajavac"\": " >> log.txt
+echo "Ошибки \"javac "$file_name_with_extension"\": " >> log.txt
 echo "--------------------------------------------------------------------" >> log.txt
 javac "$file_name_with_extension" 2>> log.txt
 javac "$file_name_with_extension" 
@@ -41,22 +42,22 @@ if [ $? -eq 0 ]; then
 	echo "Переменная @oshibka@ = 0 так как ошибок нету" >> log.txt
 	echo "--------------------------------------------------------------------" >> log.txt
 	echo -e "" >> log.txt
-	echo "\"javac "$namefiledlajavac"\" выдала ошибки?" >> log.txt
+	echo "\"javac "$file_name_with_extension"\" выдала ошибки?" >> log.txt
 	echo "Нет" >> log.txt
 	echo "Если переменная @oshibka@ = 0 то" >> log.txt
 	if [ 0 = "$oshibka" ]; then
 		clear
 		echo "Отчищаем консоль" >> log.txt
 	fi
-	java "$namefiledlajava"
+	java "$file_name_without_extension"
 	
 else
 	echo "--------------------------------------------------------------------"
 	echo "--------------------------------------------------------------------" >> log.txt
 	echo -e "" >> log.txt
-	echo "\"javac "$namefiledlajavac"\" выдала ошибки?" >> log.txt
+	echo "\"javac "$file_name_with_extension"\" выдала ошибки?" >> log.txt
 	echo "Да" >> log.txt
-	echo "Ошибка в \"javac "$namefiledlajavac"\""
+	echo "Ошибка в \"javac "$file_name_with_extension"\""
 
 fi
 
